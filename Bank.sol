9b5d05def98eb6cac6bfe2c0eb8f3bfdf72ae867
tusharmahajan8359 <tusharmahajan8359@gmail.com>
2022-02-24 11:54:06 +0530

    spacing

--- Bank.sol
+++ Bank.sol
@@ -2,42 +2,56 @@
  
 pragma solidity >=0.5.0<0.9.0;
 
-contract Bank{
+contract Bank
+     {
 
-  mapping(address=>uint) user;
+          mapping(address=>uint) user;
 
-  function deposit() payable public{
-    user[msg.sender]+= msg.value;
-  }
-  function withdraw(uint e)  public returns(string memory)
-  {
-   if(address(msg.sender).balance<(e*(10**18)))
-   {
-     return "Not enough balance";
-   }
-   else
-   {
-    payable(msg.sender).transfer(e*(10**18));
-   }
-  return "sucessful withdraw";
-  }
-  function My_balance() public view returns(uint){
-    return address(msg.sender).balance;
-  }
-  function Bank_Wallet() public view returns(uint){
-    return address(this).balance;
-  }
-  function transfer(address user1 ,uint e) public  returns(string memory){
-   
+      function deposit() payable public
+      {
+         user[msg.sender]+= msg.value;
 
-    if(address(msg.sender).balance<(e*(10**18)))
-   {
-     return "Not enough balance";
-   }
-   else
-   {
-    payable(user1).transfer(e*(10**18));
-   }
-    return " Transfer sucessful";
-  }
+      }
+
+      function withdraw(uint e)  public returns(string memory)
+      {
+        if(address(msg.sender).balance<(e*(10**18)))
+        {
+          return "Not enough balance";
+        }
+        else
+        {
+         payable(msg.sender).transfer(e*(10**18));
+        }
+     
+       return "sucessful withdraw";
+     
+      }
+
+     function My_balance() public view returns(uint)
+     {
+         return address(msg.sender).balance;
+     }
+
+     function Bank_Wallet() public view returns(uint)
+     {
+         return address(this).balance;
+     }
+     
+     function transfer(address user1 ,uint e) public  returns(string memory)
+     {
+        
+         if(address(msg.sender).balance<(e*(10**18)))
+        {
+          return "Not enough balance";
+        }
+        else
+        {
+         payable(user1).transfer(e*(10**18));
+        }
+         
+     return " Transfer sucessful";
+  
+     }
+
 }
