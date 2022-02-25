9dae9d7f21678f5ab5b3d0969f7294030b2e6235
tusharmahajan8359 <tusharmahajan8359@gmail.com>
2022-02-25 11:42:54 +0530

    Modify transfer operation

--- Bank.sol
+++ Bank.sol
@@ -1,57 +1,42 @@
 // SPDX-License-Identifier: GPL-3.0;
- 
 pragma solidity >=0.5.0<0.9.0;
 
-contract Bank
-     {
+//0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
 
-          mapping(address=>uint) user;
+contract Bank{
 
-      function deposit() payable public
-      {
-         user[msg.sender]+= msg.value;
+  mapping(address=>uint) user;
 
-      }
-
-      function withdraw(uint e)  public returns(string memory)
-      {
-        if(address(msg.sender).balance<(e*(10**18)))
-        {
-          return "Not enough balance";
-        }
-        else
-        {
-         payable(msg.sender).transfer(e*(10**18));
-        }
+  function deposit() payable public{
+    user[msg.sender]+= msg.value;
+  }
+  function withdraw(uint e)  public returns(string memory)
+  {
+    uint amount=e*(10**18);
+    require(address(msg.sender).balance>(amount), "Not enough balance");
+    user[msg.sender]-=amount;
+    payable(msg.sender).transfer(e*(10**18));
+   
+  return "sucessful withdraw";
+  }
+  function My_balance() public view returns(uint){
+    return address(msg.sender).balance;
+  }
+  function MyBank_Wallet() public view returns(uint){
+    return user[msg.sender];
+  }
+  function transfer(address user1 ,uint e) public  returns(string memory){
      
-       return "sucessful withdraw";
-     
-      }
+      uint amount=e*(10**18);
+      require(address(msg.sender).balance>(amount) , "Not enough balance");
+      user[msg.sender]-=amount;
 
-     function My_balance() public view returns(uint)
-     {
-         return address(msg.sender).balance;
-     }
 
-     function Bank_Wallet() public view returns(uint)
-     {
-         return address(this).balance;
-     }
-     
-     function transfer(address user1 ,uint e) public  returns(string memory)
-     {
-        
-         if(address(msg.sender).balance<(e*(10**18)))
-        {
-          return "Not enough balance";
-        }
-        else
-        {
-         payable(user1).transfer(e*(10**18));
-        }
-         
-     return " Transfer sucessful";
-  
-     }
+      user[user1]+=amount;              //wallet to wallet
+     //payable(user1).transfer(amount); // wallet to aur amount
 
+    
+   
+    return " Transfer sucessful";
+  }
 }
